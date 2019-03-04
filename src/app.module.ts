c93be081ea9d26aea3264d9e52ddb7f9ad942ff6
Jay Dan Howard <hello@jaydanhoward>
2019-03-04 00:42:05 -0800

    Add user module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ArticleModule } from './article/article.module';
+import { UserModule } from './users/user.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/articles', {
       useNewUrlParser: true,
     }),
+    MongooseModule.forRoot('mongodb://localhost/user', {
+      useNewUrlParser: true,
+    }),
     ArticleModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
