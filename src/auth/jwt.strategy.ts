fe1f79ce6fbb71b4d5e5c45f3317b579fdba595f
Jay Howard <hello@jaydanhoward.com>
2019-03-05 10:57:45 -0800

    Apply secretkey dependency to jwt strat

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -3,13 +3,13 @@ import { AuthService } from './auth.service';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtPayload } from './interfaces/jwt-payload.interface';
-
+import { JwtSecretKey } from '../keys';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly authService: AuthService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'secretKey',
+      secretOrKey: JwtSecretKey,
     });
   }
 
