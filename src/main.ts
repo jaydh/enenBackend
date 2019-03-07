ffd7ebc9a928c7654d5550869ce2682684cd0343
Jay Howard <hello@jaydanhoward.com>
2019-03-07 11:08:50 -0800

    Add https options

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { sslSecret, sslRoute } from './keys';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: sslSecret,
+      cert: sslRoute,
+    },
+  });
   app.enableCors();
   await app.listen(3000);
 }
