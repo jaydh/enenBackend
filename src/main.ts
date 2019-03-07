e3a7c56a694dc4f029ddbac5dcb48c78be95da18
Jay Howard <hello@jaydanhoward.com>
2019-03-07 11:37:00 -0800

    Add pem to do ssl

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { sslSecret, sslRoute } from './keys';
+import * as fs from 'fs';
+import * as https from 'https';
+import * as pem from 'pem';
+import * as express from 'express';
 
-async function bootstrap() {
+pem.createCertificate({ days: 1, selfSigned: true }, (err, keys) => {
+  if (err) {
+    throw err;
+  }
+  bootstrap(keys);
+});
+
+async function bootstrap(keys: { serviceKey: any; certificate: any }) {
   const app = await NestFactory.create(AppModule, {
     httpsOptions: {
-      key: sslSecret,
-      cert: sslRoute,
+      key: keys.serviceKey,
+      cert: keys.certificate,
     },
   });
   app.enableCors();
   await app.listen(3000);
 }
-bootstrap();
