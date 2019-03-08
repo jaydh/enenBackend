6c7eb202a4771f84706fbeae292423fe3eafb391
Jay Howard <hello@jaydanhoward.com>
2019-03-08 00:04:25 -0800

    Remove pem

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as fs from 'fs';
 import * as https from 'https';
-import * as pem from 'pem';
 import * as express from 'express';
 
-pem.createCertificate({ days: 1, selfSigned: true }, (err, keys) => {
-  if (err) {
-    throw err;
-  }
-  bootstrap(keys);
-});
-
-async function bootstrap(keys: { serviceKey: any; certificate: any }) {
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions: {
-      key: keys.serviceKey,
-      cert: keys.certificate,
-    },
-  });
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {});
   app.enableCors();
   await app.listen(4300);
 }
+bootstrap();
