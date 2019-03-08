faf39cd82ce3b81bfcad426fd2dc0f71f78d1856
Jay Howard <hello@jaydanhoward.com>
2019-03-08 00:00:43 -0800

    Add healthcheck

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('health')
+  async getHealth(@Res() res) {
+    return res.status(HttpStatus.OK);
+  }
 }
