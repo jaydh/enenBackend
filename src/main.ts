257a89f3030958f3ef75c057fe460d2004a76e4b
Jay Howard <hello@jaydanhoward.com>
2019-03-08 11:28:41 +0000

    SSL cert working

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as fs from 'fs';
 import * as https from 'https';
-import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {});
+  const keyFile = fs.readFileSync(__dirname + '/ssl/privkey.pem');
+  const certFile = fs.readFileSync(__dirname + '/ssl/cert.pem');
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: keyFile,
+      cert: certFile,
+    },
+  });
+
   app.enableCors();
   await app.listen(4300);
 }
